9cc04be29d03a89f873c33b148df2b141fb5c4b2
rb2001 <rbaird2001@yahoo.com>
2019-11-27 15:04:02 -0700

    functional IGDB tested

--- controllers/apiRoute.js
+++ controllers/apiRoute.js
@@ -1,8 +1,18 @@
-const MediaItem = require("../models/mediaItem")
-const igdb = require("../services/igdb")
-module.exports = function(app){
-    app.get("/games/:id",async (res,req)=>{
-        const games =  await igdb.post(`/games","fields *; where id = ${req.params.id}`)
-    })};
-
-    
+//const MediaItem = require("../models/mediaItem")
+const igdb = require("../services/igdb");
+module.exports = function(app) {
+  app.get("/games/:id", async (req, res) => {
+    try {
+      const game = await igdb.post(
+        "/games",
+        `fields *; where id = ${req.params.id};`
+      );
+      const { url } = (
+        await igdb.post("/covers", `fields url; where game = ${req.params.id};`)
+      ).data[0] || { url: "//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.jennybeaumont.com%2Fwp-content%2Fuploads%2F2015%2F03%2Fplaceholder.gif&f=1&nofb=1" };
+      res.json(game.data.map(g => ({ ...g, cover: `https:${url}` })));
+    } catch (err) {
+      console.log(err.response && err.response.data ? err.response.data : err);
+    }
+  });
+};
--- services/igdb.js
+++ services/igdb.js
@@ -2,10 +2,11 @@ const axios = require("axios")
 
 module.exports = axios.create(
     {
-        baseURL: "https://api-v3.igdb.com/" ,
+        baseURL: "https://api-v3.igdb.com" ,
         headers:{
             "Accept" : "application/json",
-            "user-key" : process.env.IGDB_KEY,
+            "user-key" :"232f73103bae48c5bc816211a0e4bc5a",
+            'Content-Type': 'text/plain'
         },
 
     }
