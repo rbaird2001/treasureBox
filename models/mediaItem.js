e56058f81d08379d18334447af20cafae6b7552d
rb2001 <rbaird2001@yahoo.com>
2019-11-26 20:35:44 -0700

    base structure of DB and routing API

---
+++ models/mediaItem.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = ("mongoose.Schema");
+
+
+const MediaItem = new Schema({
+    title: {type:string,required:true,},
+    class:{type:string,required:true,},
+    upc:{type:string,},
+    rating:{type:string,},
+    mediaType:{type:string,},
+    platform:{type:string,},
+    genre:{type:string,},
+    description:{type:string,},
+    covertArt:{type:string},
+});
+
+module.exports = MediaItem;
+
