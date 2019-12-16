7f5545073f971ca545b2ce30296658b1bf7082f1
rb2001 <rbaird2001@yahoo.com>
2019-12-16 13:53:31 -0700

    redirect variable for oAuth

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,6 @@
 var passport = require("passport");
 var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
-
+const cbUrl = process.env.NODE_LOC === "local"?"http://localHost:3000":"https://game-media-shelf.herokuapp.com"
 passport.serializeUser(function(user, done) {
  done(null, user);
 });
@@ -14,7 +14,7 @@ passport.use(
   {
    clientID: process.env.GOOGLE_CLIENT_ID,
    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-   callbackURL: "http://localhost:3000/auth/google/callback"
+   callbackURL: `${cbUrl}/auth/google/callback`
   },
   function(accessToken, refreshToken, profile, done) {
    var userData = {
